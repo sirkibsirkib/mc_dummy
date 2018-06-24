b5509ef3ff5bef9b4e9d74c4e9783119961d5914
sirkibsirkib <christopher.esterhuyse@gmail.com>
2018-06-24 15:49:56 +0200

    moree

---
+++ src/code_header_play.rs
@@ -0,0 +1,8 @@
+pub const LOGIN_SUCCESS: u8 	= 0x02;
+pub const SET_COMPRESSION: u8 	= 0x03;
+pub const PLAYER_ABILTIES: u8	= 0x2C;
+pub const JOIN_GAME: u8			= 0x23;
+pub const PLUGIN_MSG: u8		= 0x18;
+pub const SET_SLOT: u8			= 0x16;
+pub const CLOSE_WINDOW: u8		= 0x12;
+pub const WINDOW_ITEMS: u8		= 0x14;
--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,17 @@ use std::io::BufReader;
 use clap::App;
 
 mod mc_channel;
+mod code_header_play;
 
 use mc_channel::{
-	Packet, HandshakeState,
-	ReadPlusPlus,
-	WritePlusPlus,
+    Packet, HandshakeState,
+    ReadPlusPlus,
+    WritePlusPlus,
 };
 
 
 fn main() {
-	let matches = App::new("MC_Dummy")
+    let matches = App::new("MC_Dummy")
             .version("1.0")
             .author("C. Esterhuyse <christopher.esterhuyse@gmail.com>")
             .about("Connects a Dummy Client to an offline-mode Minecraft server which will wander around.")
@@ -54,27 +55,67 @@ fn go(addr: SocketAddr) {
 
     let mut buf: Vec<u8> = vec![];
     while buf.len() < 64 {
-    	buf.push(0);
+        buf.push(0);
     }
 
+    let mut compression_thresh = None;
+
     loop {
-    	let len = stream.read_varint() as usize;
-    	while len > buf.len() {
-    		buf.push(0u8);
-    	}
-    	stream.read_exact(&mut buf[0..len]);
-    	let mut payload = BufReader::new(&buf[0..len]);
-		let code = payload.read_varint();
-		println!("len {}. code {}", len, code);
-		match code {
-			0x03 => {
-				let thresh = payload.read_varint();
-				println!("THRESH = {}", thresh);
-			},
-			x => {
-				println!("idk fam");
-			}
-		}
+        let len = stream.read_varint() as usize;
+        while len > buf.len() {
+            buf.push(0u8);
+        }
+        stream.read_exact(&mut buf[0..len]);
+
+        /*
+        We wrap the payload buffer into a reader.
+        its now impossible to read out of bounds and calls to payload
+        will advance the state of the reader (`payload`) as expected
+        */
+        let mut payload = BufReader::new(&buf[0..len]);
+
+        let code: u8 = match compression_thresh {
+            None => {
+                let x = payload.read_varint();
+                assert!(0 <= x && x <= 256);
+                x as u8
+            },
+            Some(k) => {
+                let compress_code = payload.read_varint();
+                println!("compress_code {}", compress_code);
+                if compress_code != 0 {
+                    continue;
+                }
+                let x = payload.read_varint();
+                assert!(0 <= x && x <= 256);
+                x as u8
+            }   
+        };
+
+        println!("len {}. code {}", len, code);
+        use code_header_play::*;
+        match code {
+            SET_COMPRESSION => {
+                let thresh = payload.read_varint();
+                println!("THRESH = {}", thresh);
+                compression_thresh = Some(thresh as u32);
+            },
+            JOIN_GAME       |
+            LOGIN_SUCCESS   |
+            PLUGIN_MSG      | 
+            SET_SLOT        |
+            CLOSE_WINDOW    |
+            WINDOW_ITEMS    |
+            PLAYER_ABILTIES => {
+                println!("idgaf.");
+            },
+            x => {
+                println!("unknown code {} (hex {})", x, hex::encode(&[x;1]));
+            }
+        };
+        let mut remainder = vec![];
+        let p = payload.read_to_end(&mut remainder).unwrap();
+        println!("remainder {}", hex::encode(&remainder[0..p]));
     }
 }
 
